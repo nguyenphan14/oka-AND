5a0139c6353c2da5389e3478f5efe596f76a490f
nguyenph <nguyenph.ptit@gmail.com>
2023-04-06 16:19:10 +0700

    continue config server for running purpose

--- comment_service/comment_service/settings.py
+++ comment_service/comment_service/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'comment_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'commentservice',
+        'USER': 'root',
+        'PASSWORD': 'PhanNguyen1411@',
+        'PORT': '3306'
     }
 }
 
